b79b3c601c12ab707a414610d2fbd44c81f77534
NEBULA1978 <rpascualey@gmail.com>
2023-10-23 00:44:27 +0200

    Añadiendo funcionalidades con numeros para introducir opciones

--- bash-menu-dinamico3-final4.sh
+++ bash-menu-dinamico3-final4.sh
@@ -12,20 +12,30 @@ while true; do
 
     case $choice in
         1)
-            ls "$(pwd)"
-            read -p "Introduzca el nombre del archivo de búsqueda: " buscar_file
+            files=($(ls))
+            for i in "${!files[@]}"; do
+                echo "$i. ${files[$i]}"
+            done
+            read -p "Introduzca el número del archivo de búsqueda: " buscar_option
+            buscar_file="${files[$buscar_option]}"
             cat -n "$buscar_file"
             ;;
         2)
-            read -p "Introduzca el nombre del archivo de búsqueda: " buscar_file
-            read -p "Introduzca el nombre del archivo de destino: " destino_file
+            files=($(ls))
+            for i in "${!files[@]}"; do
+                echo "$i. ${files[$i]}"
+            done
+            read -p "Introduzca el número del archivo de búsqueda: " buscar_option
+            buscar_file="${files[$buscar_option]}"
+            read -p "Introduzca el número del archivo de destino: " destino_option
+            destino_file="${files[$destino_option]}"
             read -p "Introduzca el número de la primera línea del rango a copiar: " start_line
             read -p "Introduzca el número de la última línea del rango a copiar: " end_line
             read -p "Introduzca el número de línea en el archivo de destino donde desea introducir la copia: " destination_line
 
             # Copiar el rango de líneas al archivo temporal
             sed -n "${start_line},${end_line}p" "$buscar_file" > temp_copy.txt
-            
+
             # Insertar el contenido copiado en el archivo de destino
             head -n $((destination_line - 1)) "$destino_file" > temp_head.txt
             tail -n +$destination_line "$destino_file" > temp_tail.txt
@@ -35,19 +45,34 @@ while true; do
             echo "Rango de líneas copiado exitosamente en la línea $destination_line."
             ;;
         3)
-            read -p "Introduzca el nombre del archivo de introducción: " introducir_file
+            files=($(ls))
+            for i in "${!files[@]}"; do
+                echo "$i. ${files[$i]}"
+            done
+            read -p "Introduzca el número del archivo de introducción: " introducir_option
+            introducir_file="${files[$introducir_option]}"
             read -p "Introduzca la palabra a eliminar: " word_to_remove
             sed -i "7,8s/${word_to_remove}//" "$introducir_file"
             echo "Palabra '${word_to_remove}' eliminada de las líneas 7 y 8."
             ;;
         4)
-            read -p "Introduzca el nombre del archivo de introducción: " introducir_file
+            files=($(ls))
+            for i in "${!files[@]}"; do
+                echo "$i. ${files[$i]}"
+            done
+            read -p "Introduzca el número del archivo de introducción: " introducir_option
+            introducir_file="${files[$introducir_option]}"
             read -p "Introduzca el nuevo título: " new_title
             sed -i "s/<title>.*<\/title>/<title>${new_title}<\/title>/" "$introducir_file"
             echo "Título modificado en $introducir_file."
             ;;
         5)
-            read -p "Introduzca el nombre del archivo de introducción: " introducir_file
+            files=($(ls))
+            for i in "${!files[@]}"; do
+                echo "$i. ${files[$i]}"
+            done
+            read -p "Introduzca el número del archivo de introducción: " introducir_option
+            introducir_file="${files[$introducir_option]}"
             read -p "Introduzca el número de línea a eliminar: " line_number
             sed -i "${line_number}d" "$introducir_file"
             echo "Línea eliminada exitosamente."
